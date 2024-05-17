2dc80574e270ca05fe6e0fd3edd3aa6bdbbc651e
Gabriel D Minzemalulu <gabriel.CSCU.2018@gmail.com>
2024-05-17 18:20:21 +0300

    "Refactor PersonalInfo component: rename and split into child-personal-info and mother-personal-info, update imports in Profiles/Child and Profiles/Mother pages"

--- app/(dashboard)/Profiles/Child/page.tsx
+++ app/(dashboard)/Profiles/Child/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 // components/Profile.tsx
 import ActivityLog from "@/app/components/patient-activity-log";
-import PersonalInfo from "@/app/components/personal-info";
+import PersonalInfo from "@/app/components/child-personal-info";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
